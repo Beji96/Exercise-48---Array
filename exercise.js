d2d81e93b718f64acafe5294fdd99abf489494d5
Flavius <flaviusbejan96@yahoo.com>
2022-09-22 10:39:46 +0300

    Finish

---
+++ exercise.js
@@ -0,0 +1,7 @@
+const students = [
+  { id: 5, name: "Flavius", surname: "Bejan", age: 26 },
+  { id: 8, name: "Loek ", surname: "Konnor", age: 30 },
+  { id: 12, name: "Sandalio", surname: "Peña", age: 18 },
+];
+
+console.log(students[0]);
